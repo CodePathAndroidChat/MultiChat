01f8b1e355a082e06536d79b5963bb318bc0cd21
Alitsiya Yusupova <alitsiya.yusupova@lookout.com>
2017-10-09 19:39:36 -0700

    Add firebase support with Realtime Database, Cloud Messaging, Authentication
    - Add login activity and screen
    - Show logged user in MainActivity

---
+++ MultiChatApp/app/src/main/java/com/example/jason/multichatapp/activities/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.jason.multichatapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
+import com.example.jason.multichatapp.R;
+import com.example.jason.multichatapp.activities.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "@@@";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fabric.with(this, new Crashlytics());
+
+        // Initialize the FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+        // Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
+//
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            // Name, email address, and profile photo Url
+            String name = currentUser.getDisplayName();
+            String email = currentUser.getEmail();
+            TextView tvEmail = (TextView) findViewById(R.id.tvUserName);
+            tvEmail.setText(email);
+            // Check if user's email is verified
+//            boolean emailVerified = currentUser.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = currentUser.getUid();
+            Log.d(TAG, "User is logged " + uid);
+        } else {
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
+}
