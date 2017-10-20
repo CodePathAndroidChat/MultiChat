50492de238e95df58d32acb3a4c631e350dcb9b5
Alitsiya Yusupova <alitsiya.yusupova@lookout.com>
2017-10-19 23:15:31 -0700

    Align user message to the right

--- MultiChatApp/app/src/main/java/com/example/jason/multichatapp/adapters/MessagesAdapter.java
+++ MultiChatApp/app/src/main/java/com/example/jason/multichatapp/adapters/MessagesAdapter.java
@@ -13,9 +13,6 @@ import android.widget.TextView;
 import com.example.jason.multichatapp.R;
 import com.example.jason.multichatapp.Utils.DateTimeUtils;
 import com.example.jason.multichatapp.models.ChatMessage;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -25,17 +22,10 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
 
     private Context mContext;
     private ViewHolder mViewHolder;
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference myRef;
-    private FirebaseAuth mAuth;
 
     public MessagesAdapter(Context context, List<ChatMessage> messages) {
         mChatMessages = messages;
         mContext = context;
-
-        mDatabase = FirebaseDatabase.getInstance();
-        myRef = mDatabase.getReference("message");
-        mAuth = FirebaseAuth.getInstance();
     }
 
     private Context getContext() {
@@ -47,7 +37,11 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
         public TextView tvMessage;
         public TextView tvUserName;
         public TextView tvTimeAgo;
+        public TextView tvMyMessage;
+        public TextView tvMyUserName;
+        public TextView tvMyTimeAgo;
         public RelativeLayout rlMessage;
+        public RelativeLayout rlMyMessage;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -55,7 +49,11 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
             tvMessage = (TextView) itemView.findViewById(R.id.tvMessage);
             tvUserName = (TextView) itemView.findViewById(R.id.tvUserName);
             tvTimeAgo = (TextView) itemView.findViewById(R.id.tvTimeAgo);
+            tvMyMessage = (TextView) itemView.findViewById(R.id.tvMyMessage);
+            tvMyUserName = (TextView) itemView.findViewById(R.id.tvMyUserName);
+            tvMyTimeAgo = (TextView) itemView.findViewById(R.id.tvMyTimeAgo);
             rlMessage = (RelativeLayout) itemView.findViewById(R.id.rlMessage);
+            rlMyMessage = (RelativeLayout) itemView.findViewById(R.id.rlMyMessage);
         }
     }
 
@@ -84,18 +82,31 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
         String uid = sharedPreferences.getString("uid", null);
         if (uid != null && uid.equals(message.getName())) {
             userId = email;
+            holder.rlMessage.setVisibility(View.INVISIBLE);
+            holder.rlMyMessage.setVisibility(View.VISIBLE);
             holder.rlMessage.setGravity(Gravity.CENTER_VERTICAL | Gravity.RIGHT); // TODO set gravity to RIGHT
+            // Set item views based on your views and data model
+            TextView myTextView = mViewHolder.tvMyMessage;
+            myTextView.setText(message.getText());
+            TextView myUserName = mViewHolder.tvMyUserName;
+            myUserName.setText(userId);
+            TextView myTime = mViewHolder.tvMyTimeAgo;
+            myTime.setText(new DateTimeUtils().getRelativeTimeAgo(message.getTimestamp()));
+
         } else {
+            holder.rlMessage.setVisibility(View.VISIBLE);
+            holder.rlMyMessage.setVisibility(View.INVISIBLE);
             holder.rlMessage.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+            // Set item views based on your views and data model
+            TextView textView = mViewHolder.tvMessage;
+            textView.setText(message.getText());
+            TextView userName = mViewHolder.tvUserName;
+            userName.setText(userId);
+            TextView time = mViewHolder.tvTimeAgo;
+            time.setText(new DateTimeUtils().getRelativeTimeAgo(message.getTimestamp()));
         }
 
-        // Set item views based on your views and data model
-        TextView textView = mViewHolder.tvMessage;
-        textView.setText(message.getText());
-        TextView userName = mViewHolder.tvUserName;
-        userName.setText(userId);
-        TextView time = mViewHolder.tvTimeAgo;
-        time.setText(new DateTimeUtils().getRelativeTimeAgo(message.getTimestamp()));
+
     }
 
     @Override
